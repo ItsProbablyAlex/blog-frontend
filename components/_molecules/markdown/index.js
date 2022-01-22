f58f20d24bcdd8db3865fdd318600568d59a032a
Alexander Chapman <achapman135@gmail.com>
2022-01-22 16:20:58 +0000

    Formatted markdown links to use usual style

--- components/_molecules/markdown/index.js
+++ components/_molecules/markdown/index.js
@@ -1,8 +1,9 @@
 import { MDXRemote } from 'next-mdx-remote';
 import UndecoratedList from '../../_atoms/undecoratedlist';
 import IconLink from '../../_atoms/iconlink';
+import BaseLink from '../../_atoms/baselink';
 
-const components = {UndecoratedList, IconLink};
+const components = {UndecoratedList, IconLink, a: BaseLink};
 
 const Markdown = ({content}) => <MDXRemote {...content} components={components} />;
 
