38d8c9f4c3ebaaaf9b2b263819aa812d595a2d5f
Alexander Chapman <achapman135@gmail.com>
2022-01-22 14:40:36 +0000

    Don't prefetch links

--- components/_atoms/link/index.js
+++ components/_atoms/link/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import BaseLink from "../baselink";
 
 export default ({children, path, className}) => (
-    <Link href={path} passHref>
+    <Link href={path} prefetch={false} passHref>
         <BaseLink className={className}>{children}</BaseLink>
     </Link>
 );
